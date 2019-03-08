efad4f6671dd9a6afe47e9b43d7c5ba9cd7b7db8
Duc Hung <hung.han@notabasement.com>
2019-03-08 15:45:42 +0700

    update

--- integer_to_roman.go
+++ integer_to_roman.go
@@ -1,92 +1,13 @@
 package main
 
-import (
-	"strconv"
-	"strings"
-)
-
 func intToRoman(num int) string {
-	m := map[int]string{
-		0:    "",
-		1:    "I",
-		2:    "II",
-		3:    "III",
-		4:    "IV",
-		5:    "V",
-		6:    "VI",
-		7:    "VII",
-		8:    "VIII",
-		9:    "IX",
-		10:   "X",
-		40:   "XL",
-		50:   "L",
-		90:   "XC",
-		100:  "C",
-		400:  "CD",
-		500:  "D",
-		900:  "CM",
-		1000: "M",
-	}
 
-	result := []string{}
-	numString := strconv.Itoa(num)
-	for len(numString) > 0 {
-		trunk := ""
-		if len(numString) > 4 {
-			trunk = numString[:4]
-			numString = numString[4:]
-		} else {
-			trunk = numString[:]
-			numString = ""
-		}
+	M := []string{"", "M", "MM", "MMM"}
+	C := []string{"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"}
+	X := []string{"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"}
+	I := []string{"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"}
 
-		trunkNum, _ := strconv.Atoi(trunk)
-		str := ""
+	res := M[num/1000] + C[(num/100)%10] + X[(num/10)%10] + I[num%10]
 
-		mod := trunkNum % 10
-		str = m[mod] + str
-
-		trunkNum = trunkNum / 10
-		if trunkNum <= 0 {
-			result = append(result, str)
-			continue
-		}
-
-		mod = trunkNum % 10
-		if mod == 9 {
-			str = "XC" + str
-		} else if mod == 5 {
-			str = "L" + str
-		} else {
-			str = m[mod] + "X" + str
-		}
-
-		trunkNum = trunkNum / 10
-		if trunkNum <= 0 {
-			result = append(result, str)
-			continue
-		}
-
-		mod = trunkNum % 10
-		if mod == 9 {
-			str = "CM" + str
-		} else if mod == 5 {
-			str = "D" + str
-		} else {
-			str = m[mod] + "C" + str
-		}
-
-		trunkNum = trunkNum / 10
-		if trunkNum <= 0 {
-			result = append(result, str)
-			continue
-		} else if trunkNum == 1 {
-			str = "M" + str
-		} else {
-			str = m[trunkNum] + "M" + str
-		}
-
-		result = append(result, str)
-	}
-	return strings.Join(result, ",")
+	return res
 }

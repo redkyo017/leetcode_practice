23daee09e770a5b8e8ea1f615744f689e23e50b6
Duc Hung <hung.han@notabasement.com>
2019-03-22 18:01:28 +0700

    update

--- 3sum.go
+++ 3sum.go
@@ -5,6 +5,19 @@ import "log"
 func threeSum(nums []int) [][]int {
 	result := [][]int{}
 	log.Println(nums)
+	setNum := make(map[int]int)
+	for _, num := range nums {
+		setNum[num] = num
+	}
+	for i := 0; i < len(setNum); i++ {
+		for j := i + 1; j < len(setNum); j++ {
+			for k := j + 1; k < len(setNum); k++ {
+				if setNum[i]+setNum[j]+setNum[k] == 0 {
+					result = append(result, []int{setNum[i], setNum[j], setNum[k]})
+				}
+			}
+		}
+	}
 	return result
 }
 

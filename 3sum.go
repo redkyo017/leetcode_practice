19eb23db17ac8860ffd146a37e6d8d178d327d8b
Duc Hung <hung.han@notabasement.com>
2019-03-25 14:45:36 +0700

    update

--- 3sum.go
+++ 3sum.go
@@ -1,19 +1,34 @@
 package main
 
-import "log"
+import (
+	"sort"
+)
 
 func threeSum(nums []int) [][]int {
 	result := [][]int{}
-	log.Println(nums)
-	setNum := make(map[int]int)
-	for _, num := range nums {
-		setNum[num] = num
-	}
-	for i := 0; i < len(setNum); i++ {
-		for j := i + 1; j < len(setNum); j++ {
-			for k := j + 1; k < len(setNum); k++ {
-				if setNum[i]+setNum[j]+setNum[k] == 0 {
-					result = append(result, []int{setNum[i], setNum[j], setNum[k]})
+	sort.Ints(nums)
+
+	for i := 0; i < len(nums)-2; i++ {
+		if i == 0 || (i > 0 && nums[i] != nums[i-1]) {
+			lo := i + 1
+			hi := len(nums) - 1
+			sum := 0 - nums[i]
+
+			for lo < hi {
+				if nums[lo]+nums[hi] == sum {
+					result = append(result, []int{nums[i], nums[lo], nums[hi]})
+					for lo < hi && nums[lo] == nums[lo+1] {
+						lo++
+					}
+					for lo < hi && nums[hi] == nums[hi-1] {
+						hi--
+					}
+					lo++
+					hi--
+				} else if nums[lo]+nums[hi] < sum {
+					lo++
+				} else {
+					hi--
 				}
 			}
 		}
